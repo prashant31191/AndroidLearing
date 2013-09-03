0cf3fed5f7dfaec0eb8bad62d026be9007d1f7d1
cashow <wy89767@163.com>
2013-09-03 18:12:57 +0800

    fix a bug

--- DownloadImageToListview/src/com/example/downloadimagetolistview/Util.java
+++ DownloadImageToListview/src/com/example/downloadimagetolistview/Util.java
@@ -18,15 +18,16 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Environment;
 import android.util.Log;
+import android.widget.Toast;
 
 public class Util {
 	public static final String SAVE_PATH = Environment
 			.getExternalStorageDirectory() + "/DownloadImagesFromListview/";
-//	public static final int MAXSIZE = 1048576; 			// 限定只能使用10MB的内存
-	public static final int MAXSIZE = 90000; 			// 限定只能使用90000KB的内存
-	public static final int EXPIREDTIME = 86400000;		// 图片资源的有效期为24小时
+	public static final int MAXSIZE = 1048576; // 限定只能使用10MB的内存
+	// public static final int MAXSIZE = 90000; // 限定只能使用90000KB的内存
+	public static final int EXPIREDTIME = 86400000; // 图片资源的有效期为24小时
 
-	//保存bitmap到sd卡上
+	// 保存bitmap到sd卡上
 	public static void saveimage(Bitmap mBitmap, String fileName)
 			throws IOException {
 		File f = new File(SAVE_PATH + fileName);
@@ -42,110 +43,77 @@ public class Util {
 			e.printStackTrace();
 		}
 
-		/*if (getDirectorySize() > MAXSIZE) {
-			removeUnusedFile();
-		}*/
+		/*
+		 * if (getDirectorySize() > MAXSIZE) { removeUnusedFile(); }
+		 */
 	}
 
-	//当文件夹大小超过限定的10MB时，清除%40最近没有被使用的文件
-	/*private static void removeUnusedFile() {
-		File dir = new File(SAVE_PATH);
-		File[] files = dir.listFiles();
-		if (files == null) {
-			return;
-		}
-		int removeNumber = (int)((0.4*files.length)+1);
-		Arrays.sort(files, new FileLastModifSort());
-		for (int i = 0; i < removeNumber; i++) {
-				files[i].delete();
-		}
-	}
-	
-	private static void removeExpiredCache(String filename) {
-	    File file = new File(SAVE_PATH,filename);
-	    if(!file.exists())
-	    	return;
-	    if (System.currentTimeMillis() - file.lastModified() > EXPIREDTIME) {
-	        file.delete();
-	    }
-	}*/
-	
-	//用于文件的排序，比较文件的修改时间
-	public static class FileLastModifSort implements Comparator<File>{
-	    public int compare(File arg0, File arg1) {
-	        if (arg0.lastModified() >arg1.lastModified()) {
-	            return 1;
-	        } else if (arg0.lastModified() ==arg1.lastModified()) {
-	            return 0;
-	        } else {
-	            return -1;
-	        }
-	    }
-	}
+	// 当文件夹大小超过限定的10MB时，清除%40最近没有被使用的文件
+	/*
+	 * private static void removeUnusedFile() { File dir = new File(SAVE_PATH);
+	 * File[] files = dir.listFiles(); if (files == null) { return; } int
+	 * removeNumber = (int)((0.4*files.length)+1); Arrays.sort(files, new
+	 * FileLastModifSort()); for (int i = 0; i < removeNumber; i++) {
+	 * files[i].delete(); } }
+	 * 
+	 * private static void removeExpiredCache(String filename) { File file = new
+	 * File(SAVE_PATH,filename); if(!file.exists()) return; if
+	 * (System.currentTimeMillis() - file.lastModified() > EXPIREDTIME) {
+	 * file.delete(); } }
+	 */
 
-	//获取文件夹的大小
-	/*private static long getDirectorySize() {
-		File dirFile = new File(SAVE_PATH);
-		long size = 0;
-		if (dirFile.exists()) {
-			File files[] = dirFile.listFiles();
-			for (int i = 0; i < files.length; i++) {
-				size += files[i].length();
+	// 用于文件的排序，比较文件的修改时间
+	public static class FileLastModifSort implements Comparator<File> {
+		public int compare(File arg0, File arg1) {
+			if (arg0.lastModified() > arg1.lastModified()) {
+				return 1;
+			} else if (arg0.lastModified() == arg1.lastModified()) {
+				return 0;
+			} else {
+				return -1;
 			}
-			return size;
 		}
-		return 0;
-	}*/
+	}
+
+	// 获取文件夹的大小
+	/*
+	 * private static long getDirectorySize() { File dirFile = new
+	 * File(SAVE_PATH); long size = 0; if (dirFile.exists()) { File files[] =
+	 * dirFile.listFiles(); for (int i = 0; i < files.length; i++) { size +=
+	 * files[i].length(); } return size; } return 0; }
+	 */
 
 	public static Bitmap getimagefromurl(String path) {
 		try {
-			Bitmap mBitmap;
+			Bitmap mBitmap = null;
 			String filename = Util.md5(path) + ".png";
 			File file = new File(Util.SAVE_PATH + filename);
-			//removeExpiredCache(filename);
+			// removeExpiredCache(filename);
+			
+			//如果不存在文件或者文件正在下载中，则mBitmap为空
 			if (file.exists()) {
-				//假如本地有这个图片文件，则直接去本地读取
-				long curlen = file.length();
-				long prelen = file.length();
-				while (true) {
-					try {
-						Thread.sleep(500);
-						curlen = file.length();
-						Log.d("wy",prelen+" -> "+curlen);
-						if (curlen == prelen) {
-							break;
-						} else {
-							prelen = curlen;
-						}
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-				}
-				return Util.ReadFromSdcard(filename);
-			} else {
-				//在本地妹找到这个图片，则去网上根据url下载对应的图片
-				URL url = new URL(path);
-				HttpURLConnection conn;
-				conn = (HttpURLConnection) url.openConnection();
-				conn.setConnectTimeout(5 * 1000); // 设置连接的时限为5秒
-				conn.setRequestMethod("GET");
+				mBitmap = BitmapFactory.decodeFile(SAVE_PATH + path);
+			}
+			if (mBitmap != null){
+				return mBitmap;
+			}
+			
+			// 在本地没找到这个图片，则去网上根据url下载对应的图片。同一个url可能会下载多次
+			URL url = new URL(path);
+			HttpURLConnection conn;
+			conn = (HttpURLConnection) url.openConnection();
+			conn.setConnectTimeout(5 * 1000); // 设置连接的时限为5秒
+			conn.setRequestMethod("GET");
 
-				if (conn.getResponseCode() == HttpURLConnection.HTTP_OK) {
-					mBitmap = BitmapFactory.decodeStream(conn.getInputStream());
-					Util.saveimage(mBitmap, filename);
-					return mBitmap;
-				}
+			if (conn.getResponseCode() == HttpURLConnection.HTTP_OK) {
+				mBitmap = BitmapFactory.decodeStream(conn.getInputStream());
+				Util.saveimage(mBitmap, filename);
+				return mBitmap;
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		return null;
-	}
-
-	public static Bitmap ReadFromSdcard(String path) {
-		String imgFilePath = SAVE_PATH + path;
-		Bitmap mBitmap = BitmapFactory.decodeFile(imgFilePath);
-		return mBitmap;
 	}
 
 	public static void makedir() {
